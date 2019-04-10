d34ecba6e53fca221cb8bf62545113c6177914a2
AntonioHidalgoLanda <hidalgo.landa@gmail.com>
2019-04-10 12:07:28 +0100

    developing Intentions and Goals stub

--- JSObjects/goal.js
+++ JSObjects/goal.js
@@ -1,10 +1,11 @@
 /*global Rule*/
-function Goal(achivement, divide) {
+function Goal(achivement) {
     "use strict";
     this.priority = Goal.INITIAL_PRIORITY;
     this.duration = Goal.INITIAL_DURATION;
     this.rule_achieve = (achivement instanceof Rule) ? achivement : new Rule();
-    this.rule_divide = (divide instanceof Rule) ? divide : new Rule();
+    
+    this.intentions = {};
 }
 
 Goal.INITIAL_PRIORITY = 1;
@@ -13,17 +14,11 @@ Goal.MAX_INTENTIONS_PER_ROUND = 3;
 
 Goal.CONDITION_INSTINCT_ACHIEVEMNENT = "L.path >= goal.intensity";
 Goal.REACTION_INSTINCT_ACHIEVEMNENT = "true";        // destroy the action (true)
-Goal.CONDITION_INSTINCT_DIVIDE = "";
-Goal.REACTION_INSTINCT_DIVIDE = "";                  // urgePath() + prioritizeIntention()
 Goal.CONDITION_PATH_FOLLOWER_ACHIEVEMNENT = "";      // WorldModel.levelGreaterEqual(path, rapport_level, path_level) > people
 Goal.REACTION_PATH_FOLLOWER_ACHIEVEMNENT = "";       // destroy the action/ upgrade goals
-Goal.CONDITION_PATH_FOLLOWER_DIVIDE = "";   
-Goal.REACTION_PATH_FOLLOWER_DIVIDE = "";             // urgePath() + prioritizeIntention()
 
 Goal.RULE_INSTINCT_ACHIEVEMNENT = new Rule(Goal.CONDITION_INSTINCT_ACHIEVEMNENT, Goal.REACTION_INSTINCT_ACHIEVEMNENT);
-Goal.RULE_INSTINCT_DIVIDE = new Rule(Goal.CONDITION_INSTINCT_DIVIDE, Goal.REACTION_INSTINCT_DIVIDE);
 Goal.RULE_PATH_FOLLOWER_ACHIEVEMNENT = new Rule(Goal.CONDITION_PATH_FOLLOWER_ACHIEVEMNENT, Goal.REACTION_PATH_FOLLOWER_ACHIEVEMNENT);
-Goal.RULE_PATH_FOLLOWER_DIVIDE = new Rule(Goal.CONDITION_PATH_FOLLOWER_DIVIDE, Goal.REACTION_PATH_FOLLOWER_DIVIDE);
 
 /*
 e.g. vent-out, get-privacy, drag-attention
@@ -31,7 +26,7 @@ e.g. vent-out, get-privacy, drag-attention
 */
 Goal.createIntinct = function (path, intensity) {
     "use strict";
-    var goal = new Goal(Goal.RULE_INSTINCT_ACHIEVEMNENT, Goal.RULE_INSTINCT_DIVIDE);
+    var goal = new Goal(Goal.RULE_INSTINCT_ACHIEVEMNENT);
     
     goal.path = path;
     goal.intensity = intensity;
@@ -42,7 +37,7 @@ Goal.createIntinct = function (path, intensity) {
 
 Goal.createPathFollowers = function (people, path, rapport_level, path_level) {
     "use strict";
-    var goal = new Goal(Goal.RULE_PATH_FOLLOWER_ACHIEVEMNENT, Goal.RULE_PATH_FOLLOWER_DIVIDE);
+    var goal = new Goal(Goal.RULE_PATH_FOLLOWER_ACHIEVEMNENT);
     
     goal.people = people;
     goal.path = path;
@@ -75,57 +70,60 @@ Goal.prototype.resolve = function (rules, facts, worldModel) {
     "use strict";
     var candidate, intentions = [];
     console.log("On Development... Rule.prototype.getCandidates(facts)");
+    this.clearIntentions();
     for (var ruleid in rules){
         candidates = rules[ruleid].getCandidates(facts);
         for (var candidate_i in candidates){
             var ratting = this.getRatting(rule, candidate, worldModel);
-            if (ratting > 0.5 || intentions.length === 0) {
-                intentions.push({
-                    "ratting": ratting,
-                    "ruleid": ruleid,
-                    "candidate": candidate
-                });
-            }
+            this.addIntention(ratting, rules[ruleid], candidate);
         }
     }
+    this.executeIntentions();
+    // max priority
+    if (this.priority >= 0) {
+        this.priority--;
+    }
+    // permantent task
+    if (this.duration >= 0) {
+        this.duration--;
+    }
+
+    return this;
+};
+
+Goal.prototype.clearIntentions = function () {
+    "use strict";
+    this.intentions = {};
+};
+
+
+Goal.prototype.addIntention = function (ratting, rule, candidate) {
+    "use strict";
+    console.log("On Development... Defining criteria to add to intentions");
+    
+    if (ratting > 0.5 || this.intentions.length === 0) {
+        this.intentions.push({
+            "ratting": ratting,
+            "rule": rule,
+            "candidate": candidate
+        });
+    }
+    
+    return this;
+};
+
+Goal.prototype.executeIntentions = function () {
+    "use strict";
+    console.log("On Development....");
     // var doneSomething = false;
-    for (int i = 0; i < intentions.length && i < Goal.MAX_INTENTIONS_PER_ROUND; i++) {
+    for (var i = 0; i < intentions.length && i < Goal.MAX_INTENTIONS_PER_ROUND; i++) {
         // if (ratting + goal > Math Random) {
         //      rule.execute({},candidate)
         //      doneSomething = true;
         // }
         // if (!doneSomething) intentions[0].rule.execute(intentions[0].candidate)
     }
-    // if subgoals, add to queue
-    // reduce priority unless is -1 (max priority)
-    // reduce duration unless is -1 (permanent)
-    // return this.rule_divide.execute({}, candidate);
-};
-
-// Find actions to increase/decrease or places to increase/decrease path
-Goal.prototype.urgePath = function (path, worldModel) {
-    "use strict";
-    console.log("On Development....");
-    // Find available people in WorldModel
-    // Find available actions in WorldModel (room?)
-    // Find rooms with heuristics for increase the path
-    
-    // (if direct_action(action, room, person) ) { addIntention({action, room, person}, priority=h(path,action, room, person)) }
-    // else, add go to h(path,action, room)
-    
     return this;
-};
-
-Goal.prototype.addIntention = function () {
-    "use strict";
-    console.log("On Development....");
-    
-};
-
-Goal.prototype.executeIntentions = function () {
-    "use strict";
-    console.log("On Development....");
-    
 };
 
 Goal.prototype.getRatting = function (rule, candidate, worldModel) {
@@ -147,11 +145,13 @@ used when learing:
 Goal.prototype.updateRatting = function (rule, candidate, worldModel, feedback) {
       "use strict";
     var sample = {};
+
     Object.assign(sample, rate_extractFeatures(rule));
     Object.assign(sample, rate_extractFeatures(candidate));
     rate_normalizeFeatures(sample);
     rate_updateNeighbour(worldModel, sample, feedback);
     rate_alignment(); // re-clustering, condensation and removing less relevant nodes
+
     return this;
   
 };
